1ef786a61a9e99dc8416d0a9593f72e89b2fc5a8
CodeBender <157215759+codeBender007@users.noreply.github.com>
2024-09-25 17:01:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import UpdateProfilePage from "./pages/UpdateProfilePage"
 import CreatePost from "./components/CreatePost"
 import ChatPage from "./pages/ChatPage"
 
-const API_BASE_URL = 'http://localhost:3000'
+const API_BASE_URL = 'https://mohdadnan.onrender.com'
 
 function App() {
 
